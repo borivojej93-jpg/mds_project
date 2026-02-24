f6350665b63fbfa371773975fee302aba7de2601
Borivoje Jovanovic <borivoje.jovanovic@wavelabs.tech>
2026-02-24 15:53:40 +0100

    cleaned test

--- test_main.py
+++ test_main.py
@@ -1,6 +1,5 @@
 from fastapi.testclient import TestClient
 
-from app import repository
 from app.main import app
 
 client = TestClient(app)
@@ -8,10 +7,4 @@ client = TestClient(app)
 
 def test_sorting_logic():
     response = client.post("/do_balance")
-    print(response)
-    assert response.status_code == 404
-    # print("oi")
-    # print(response)
-    # print(response.json())
-    # print(repository.rack_dict)
-    # assert response.json() == {"msg": "balance finished"}
+    assert response.status_code == 200
